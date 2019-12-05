f21e2314f61b3d135d2aec80919a2df618d19eb3
Sanchez Serna <gjsanchez2@mail.mccneb.edu>
2019-12-04 20:47:14 -0600

    week-8

---
+++ week-8/lastname_calculator.py
@@ -0,0 +1,27 @@
+# Author: Richard Krasso
+# Edit: Gabriel Sanchez
+# Date: 12/4/2019
+
+
+#ADD
+def add(A, B):
+    return A + B
+    
+print("Your total addition is:", add(5,50)) 
+
+#Sub
+def subtract(A,B):
+    return A - B
+
+print( "Your total subtraction is: ",subtract(50,5))
+
+#Divide
+def divide(A,B):
+    return A / B
+
+print("Your total division is: ", divide(50,5))
+
+#Functions
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
---
+++ week-8/sanchez-hello_world.py
@@ -0,0 +1,6 @@
+# Author: Richard Krasso
+# Edit: Gabriel Sanchez
+# Date: 12/4/2019
+
+
+print("Hello World")
